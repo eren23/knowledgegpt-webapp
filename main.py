892951275200ea41ad8d3f0942aab6092d72dad3
eren23 <erenak1996@gmail.com>
2023-05-07 22:27:56 +0200

    load index unfinished

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-# Bring in deps
 import os 
 from local_config import SECRET_KEY # or from config import SECRET_KEY
-
 import streamlit as st 
-
-
 import openai
 
 openai.api_key = SECRET_KEY
+
+st.title('KnowledgeGPT Document Q&A App')
+st.header('A Basic User Interface For KnowledgeGPT')
+
+save_dir = st.text_input('Enter The Directory You Want To Save The File To ( FOR NOW PLEASE REENTER EACH TIME YOU ASK A QUESTION :())')
+if save_dir:
+    st.session_state['save_dir'] = save_dir
 
 if 'is_ran' not in st.session_state:
     st.session_state['is_ran'] = False
@@ -25,29 +28,30 @@ prompt = st.text_input('Plug in your prompt here')
 
 file_type = ''
 
-# is_ran = False
 
-# Show stuff to the screen if there's a prompt
-# if selected_file and st.session_state['is_ran'] ==False:
 if selected_file:
+    if 'save_dir' in st.session_state:  
+        save_dir = st.session_state['save_dir']
+
+    indexes_path = os.path.join('indexes', save_dir)
     
     if ".pdf" in selected_file.lower():
         
         file_type = 'pdf'
         from knowledgegpt.extractors.pdf_extractor import PDFExtractor
-        pdf_extractor = PDFExtractor( selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
+        pdf_extractor = PDFExtractor(index_path=indexes_path, pdf_file_path=selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
         
         
     elif any(substring in selected_file.lower() for substring in [".ppt", ".pptx"]):
         
         file_type = 'ppt'
-        ppt_extractor = PowerpointExtractor(selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
-        
+        ppt_extractor = PowerpointExtractor(index_path=indexes_path, file_path=selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
         from knowledgegpt.extractors.powerpoint_extractor import PowerpointExtractor
         st.write("You selected a PPT file:", selected_file)
+        
     elif any(substring in selected_file.lower() for substring in [".doc", ".docx"]):
         file_type = 'doc'
-        doc_extractor = DocsExtractor(selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
+        doc_extractor = DocsExtractor(index_path=indexes_path, file_path=selected_file, extraction_type="paragraph", embedding_extractor="hf", model_lang="en")
         from knowledgegpt.extractors.docs_extractor import DocsExtractor
 
         # Do something with the selected DOC file
